feaba763894d7ac9c9096e0e621fb0255ff1cd0d
Jose Juan Jaramillo Polo <josejuan.jaramillopolo@gmail.com>
2021-03-12 23:21:54 -0600

    First Commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# ==================================================================================
+# File Name: main.py
+# ==================================================================================
+# Purpose:         Prepares almost all the basics to start creating a GUI such as:
+#                      * Logging
+#                      * Config parser (to save and load data in .ini files)
+#                      * Basic Tkinter GUI
+#                      * Pinger (to test connection with a host, server, etc...)
+#
+#
+# Author:          Jose Juan Jaramillo Polo
+# Notes:             
+# ==================================================================================
+
+
+# Name:            Main
+# Purpose:         Starts the main function from the main file
+# Parameter:       None                    
+def main():
+    print("Building...")
+
+main()
